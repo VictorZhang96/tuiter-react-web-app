531cb8bdf39f443ea2de280fe3615b05ba5099d1
VictorZhang96 <zhangxucheng01@163.com>
2022-10-21 06:43:33 -0400

    Modified tuiter part

---
+++ public/tuiter/WhoToFollowList/index.js
@@ -0,0 +1,11 @@
+import WhoToFollowListItem from "./WhoToFollowListItem.js";
+import who from './who.js';
+const WhoToFollowList = () => {
+    return (`
+       <ul class="list-group">
+        ${who.map(whoItem => {
+            return(WhoToFollowListItem(whoItem));
+        }).join('')}
+       </ul>
+`); }
+export default WhoToFollowList;
